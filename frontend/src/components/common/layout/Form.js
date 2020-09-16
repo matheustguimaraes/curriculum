1e12243b54cce102d52edbe15874394560391965
Matheus Guimaraes <matheusguimr@gmail.com>
2020-09-16 17:18:37 -0300

    Add Form.js

---
+++ frontend/src/components/common/layout/Form.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const HeaderForm = styled.form`
+  flex-direction: row;
+`;
+
+export const HeaderInput = styled.input`
+  line-height: 35px;
+  width: 100%;
+  text-align: left;
+  border-width: 1px;
+  border-style: solid;
+  border-image: initial;
+  border-radius: 4px;
+  outline: 0px;
+`;
+
+export const OuterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const Description = styled.p`
+  padding: 0;
+  margin-bottom: 1rem;
+`;
