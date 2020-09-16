9a24bee191be34980bf9e198a69f1b5b45cd6563
Matheus Guimaraes <matheusguimr@gmail.com>
2020-09-16 17:30:35 -0300

    Reuse Grid.js to create grid

--- frontend/src/components/activity/Activities.js
+++ frontend/src/components/activity/Activities.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import axios from "axios";
-import styled from "styled-components";
 
 import { Button, LinkContainer } from "../common/layout/Layout";
+import {
+  ButtonDelete,
+  Wrapper,
+  Grid,
+  Item,
+  Content,
+  Stats,
+  Container,
+  Card,
+} from "../common/layout/Grid";
 
 class UserActivities extends Component {
   constructor(props) {
@@ -113,95 +122,5 @@ class UserActivities extends Component {
     );
   }
 }
-
-const ButtonDelete = styled.button`
-  cursor: pointer;
-  //   padding: 3px;
-  border-radius: 3px;
-  border: none;
-  user-select: none;
-  color: #fff;
-  background: red;
-  &:focus {
-    outline: none;
-  }
-  &:disabled {
-    background: gray;
-  }
-`;
-
-const Wrapper = styled.div`
-  padding: 2rem 0;
-`;
-
-const Grid = styled.div`
-  padding: 30px;
-  display: grid;
-  align-items: center;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 8fr;
-  gap: 1.2rem 1.2rem;
-  @media (max-width: 960px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: 680px) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-const Item = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.11);
-  h4 {
-    color: ${({ theme }) => (theme === "light" ? "#212121" : "#fff")};
-  }
-  p {
-    color: ${({ theme }) => (theme === "light" ? "#707070" : "#c7c7c7")};
-  }
-`;
-
-const Content = styled.div`
-  padding: 1rem 0;
-`;
-
-const Stats = styled.div`
-  display: flex;
-  align-items: center;
-  div {
-    display: flex;
-    &:first-child {
-      margin-right: 0.5rem;
-    }
-    img {
-      margin: 0;
-    }
-    svg path {
-      fill: white;
-    }
-    span {
-      color: white;
-      margin-left: 0.5rem;
-    }
-  }
-`;
-
-const Container = styled.div`
-  margin: auto;
-  width: 50%;
-  @media (min-width: 601px) {
-    width: 90%;
-  }
-  @media (min-width: 993px) {
-    width: 80%;
-  }
-`;
-
-const Card = styled.div`
-  padding: 1rem;
-  background: black;
-  height: 100%;
-`;
 
 export default UserActivities;
